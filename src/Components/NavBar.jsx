27ca2a5b8b99a5c7919402f1d7572ca6b5698ed2
Benjamin <goosta19802@gmail.com>
2023-06-05 12:28:01 -0300

    as

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
   return (
     <div className="container">
     <div className="header-bar">
-      <h1 className="logo" ><a href="/">E</a></h1>
+      <h1 className="logo" ><a href="/">Eaaa</a></h1>
       <ul className="slider-menu">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/tienda">Tienda</Link></li>
